6c12ce02bd4882fbb613a4a09515535cd67847cb
Lucas Vocos <lucas.vocos@me.com>
2020-02-24 17:32:57 -0500

    Footer hover function

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,25 @@ import './Footer.scss'
 
 
 const Footer = props => {
-	// run graphql queries, useState, etc.
+
+	const [visible, setVisible] = useState(false)
+	
 	return (
-		<footer>
+		<footer className={`footer ${visible ? 'visible' : ''} ${props.isFullScreen ? 'fullscreen' : ''}`}>
+			{props.credits && (
+				<ul className={`credits${visible ? ' visible' : ''}`}>
+					{props.credits.map(credit => {
+						return <li key={credit._key}>
+							<span className='task'>{credit.task}</span>
+							{' '}
+							<span className='name'>{credit.name}</span>
+						</li>
+					})}
+				</ul>
+			)}
+			<button className='hover' 
+			onMouseEnter={() => setVisible(true)} 
+			onMouseLeave={() => setVisible(false)}></button>
 		</footer>
 	)
 		
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 import PropTypes from "prop-types"
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
@@ -8,9 +9,48 @@ import './Portfolio.scss'
 
 
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    {
+      allSanityProject {
+        nodes {
+          credits {
+            _key
+            _type
+            task
+            name
+          }
+          isFullBleed
+          title
+          projectAsset {
+            _key
+            _type
+            image {
+              _key
+              asset {
+                source {
+                  url
+                }
+              }
+            }
+            video {
+              _key
+              asset {
+                url
+                source {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  
+
   const [visible, setVisible] = useState(false)
-  const [projects] = useState(null)
-  const [currentProject] = useState(null)
+  const [projects] = useState(data.allSanityProject.nodes)
+  const [currentProject] = useState(data.allSanityProject.nodes[0])
 
   return (
     <main className='portfolio'>
@@ -22,7 +62,8 @@ const Layout = ({ children }) => {
             <button className='information' onClick={() => setVisible(!visible)}>Information</button>
           </span>
         </aside>
-      <Footer />
+        
+      <Footer isFullScreen={currentProject.isFullScreen} credits={currentProject.credits}/>
     </main>
   )
 }
