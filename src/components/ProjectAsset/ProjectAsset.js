a39ba77073baa8f670c0909f3a5f963454aa7370
Lucas Vocos <lucasvocos@gmail.com>
2020-03-06 12:34:32 -0500

    useState & useEffect for project asset

--- src/components/ProjectAsset/ProjectAsset.js
+++ src/components/ProjectAsset/ProjectAsset.js
@@ -4,41 +4,43 @@ import Img from "gatsby-image";
 import "./ProjectAsset.scss";
 
 const ProjectAsset = ({ asset }) => {
-  let projectAsset;
+  const [projectAsset, setProjectAsset] = useState(null);
 
-  if (asset.projectAsset.image.length > 0) {
-    projectAsset = (
-      <aside
-        className="img-assets"
-        id={asset.projectAsset.photoLayout}
-        style={{
-          backgroundImage: asset.isFullScreen
-            ? `url(${asset.projectAsset.image[0].asset.url})`
-            : null
-        }}
-      >
-        {asset.projectAsset.image.map(img => {
-          return (
-            <div
-              key={img.asset.id}
-              className={`img-asset${
-                img === asset.projectAsset.image[0] ? " mobile" : ""
-              }${asset.isFullScreen ? " fullscreen" : ""}`}
-            >
-              <Img fluid={img.asset.fluid} alt={asset.alt} />
-            </div>
-          );
-        })}
-      </aside>
-    );
-  } else {
-    projectAsset = (
-      <Video
-        src={asset.projectAsset.video.asset.url}
-        className={`video ${asset.isFullScreen ? "fullscreen" : ""}`}
-      />
-    );
-  }
+  useEffect(() => {
+    if (asset.projectAsset.image.length > 0) {
+      setProjectAsset(
+        <aside
+          className="img-assets"
+          id={asset.projectAsset.photoLayout}
+          style={{
+            backgroundImage: asset.isFullScreen
+              ? `url(${asset.projectAsset.image[0].asset.url})`
+              : null
+          }}
+        >
+          {asset.projectAsset.image.map(img => {
+            return (
+              <div
+                key={img.asset.id}
+                className={`img-asset${
+                  img === asset.projectAsset.image[0] ? " mobile" : ""
+                }${asset.isFullScreen ? " fullscreen" : ""}`}
+              >
+                <Img fluid={img.asset.fluid} alt={asset.alt} />
+              </div>
+            );
+          })}
+        </aside>
+      );
+    } else {
+      setProjectAsset(
+        <Video
+          src={asset.projectAsset.video.asset.url}
+          className={`video ${asset.isFullScreen ? "fullscreen" : ""}`}
+        />
+      );
+    }
+  }, [asset]);
 
   return (
     <>
