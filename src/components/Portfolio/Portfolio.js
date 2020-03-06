d4652594fda14c29b1ce33579c6b9fe9bae127d4
Lucas Vocos <lucasvocos@gmail.com>
2020-03-06 11:10:35 -0500

    modify styling for fullscreen & visible information

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import { Swipeable } from 'react-swipeable'
-import PropTypes from 'prop-types'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import Button from '../Button/Button'
-import ProjectAsset from '../ProjectAsset/ProjectAsset'
-import SEO from '../seo'
-import 'reset-css'
-import './Portfolio.scss'
+import React, { useState, useEffect } from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Swipeable } from "react-swipeable";
+import PropTypes from "prop-types";
+import Header from "../Header/Header";
 
+import Button from "../Button/Button";
+import ProjectAsset from "../ProjectAsset/ProjectAsset";
+import SEO from "../seo";
+import "reset-css";
+import "./Portfolio.scss";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -64,90 +63,101 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
-  
+  `);
 
-  const [visible, setVisible] = useState(false)
-  const [projects] = useState(data.allSanityProject.nodes.sort((a, b) => a.title.split('_')[0] - b.title.split('_')[0]))
-  const [currentProject, setCurrentProject] = useState(data.allSanityProject.nodes.sort((a, b) => a.title.split('_')[0] - b.title.split('_')[0])[0])
-  const [index, setIndex] = useState(0)
-  
-  
+  const [visible, setVisible] = useState(false);
+  const [projects] = useState(
+    data.allSanityProject.nodes.sort(
+      (a, b) => a.title.split("_")[0] - b.title.split("_")[0]
+    )
+  );
+  const [currentProject, setCurrentProject] = useState(
+    data.allSanityProject.nodes.sort(
+      (a, b) => a.title.split("_")[0] - b.title.split("_")[0]
+    )[0]
+  );
+  const [index, setIndex] = useState(0);
+
   const toggleLeft = () => {
-    let newIndex = index - 1
+    let newIndex = index - 1;
     if (newIndex < 0) {
-      newIndex = projects.length -1
+      newIndex = projects.length - 1;
     }
-    const element = document.querySelector('.project-asset')
-    element.classList.remove('visible')
+    const element = document.querySelector(".project-asset");
+    element.classList.remove("visible");
     setTimeout(() => {
-      setIndex(newIndex)
-      setCurrentProject(projects[newIndex])
-    },500)
-
-  }
+      setIndex(newIndex);
+      setCurrentProject(projects[newIndex]);
+    }, 500);
+  };
 
   const toggleRight = () => {
-    let newIndex = index + 1
-    if (newIndex >= projects.length ) {
-      newIndex = 0
+    let newIndex = index + 1;
+    if (newIndex >= projects.length) {
+      newIndex = 0;
     }
-    const element = document.querySelector('.project-asset')
-    element.classList.remove('visible')
+    const element = document.querySelector(".project-asset");
+    element.classList.remove("visible");
     setTimeout(() => {
-      setIndex(newIndex)
-      setCurrentProject(projects[newIndex])
-    },500)
-  }
+      setIndex(newIndex);
+      setCurrentProject(projects[newIndex]);
+    }, 500);
+  };
 
   useEffect(() => {
     if (projects.length) {
-      
       const interval = setInterval(() => {
         if (index === projects.length - 1) {
-          
-          setIndex(0)
-          setCurrentProject(projects[0])
-          return
+          setIndex(0);
+          setCurrentProject(projects[0]);
+          return;
         } else {
-          setCurrentProject(projects[index + 1])
-          setIndex(index + 1)
+          setCurrentProject(projects[index + 1]);
+          setIndex(index + 1);
         }
       }, 15000);
-      return () => clearInterval(interval)
+      return () => clearInterval(interval);
     }
   }, [projects, index]);
 
   return (
-    <main className={`portfolio${currentProject.isFullScreen ? ' fullscreen' : ''}`}>
-      <SEO title='JAROD TABER' />
-      
-      <Header visible={visible === true ? true : false}/>
-      
-      
-      
-      <aside className='title'>
-          <span className='title-container'>
-            <h1 className='jarod' onClick={() => setVisible(false)}>Jarod Taber</h1>
-            <button className='information' onClick={() => setVisible(!visible)}>Information</button>
-          </span>
-        </aside>
-      
-      
-      
+    <main
+      className={`portfolio${currentProject.isFullScreen ? " fullscreen" : ""}`}
+      id={visible ? "information" : ""}
+    >
+      <SEO title="JAROD TABER" />
+
+      <Header visible={visible === true ? true : false} />
+
+      <aside className="title">
+        <span className="title-container">
+          <h1 className="jarod" onClick={() => setVisible(false)}>
+            Jarod Taber
+          </h1>
+          <button className="information" onClick={() => setVisible(!visible)}>
+            Information
+          </button>
+        </span>
+      </aside>
+
       <Swipeable onSwipedRight={toggleLeft} onSwipedLeft={toggleRight}>
-        <Button className='left' onClick={toggleLeft}></Button>
-        <ProjectAsset asset={{ isFullScreen: currentProject.isFullScreen , projectAsset: currentProject.projectAsset, alt: currentProject.title }} />
-        <Button className='right' onClick={toggleRight}></Button>
+        <Button className="left" onClick={toggleLeft}></Button>
+        <ProjectAsset
+          asset={{
+            isFullScreen: currentProject.isFullScreen,
+            projectAsset: currentProject.projectAsset,
+            alt: currentProject.title,
+            credits: currentProject.credits
+          }}
+        />
+        <Button className="right" onClick={toggleRight}></Button>
       </Swipeable>
-      
-      <Footer isFullScreen={currentProject.isFullScreen} credits={currentProject.credits}/>
     </main>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
--- src/components/ProjectAsset/ProjectAsset.js
+++ src/components/ProjectAsset/ProjectAsset.js
@@ -1,52 +1,75 @@
-import React, {useState, useEffect} from 'react'
-import Video from './Video'
-import Img from 'gatsby-image'
-import './ProjectAsset.scss'
+import React, { useState, useEffect } from "react";
+import Video from "./Video";
+import Footer from "../Footer/Footer";
+import Img from "gatsby-image";
+import "./ProjectAsset.scss";
 
-const ProjectAsset = ({asset, onClick}) => {
-    const [loaded, setLoaded] = useState(false)
+const ProjectAsset = ({ asset, onClick }) => {
+  const [loaded, setLoaded] = useState(false);
 
-    let projectAsset
+  let projectAsset;
 
-    useEffect(() => {
-            const loadInterval = setTimeout(() => {
-                setLoaded(true)
-            }, 250);
-            const fadeInterval = setTimeout(() => {
-                setLoaded(false)
-            }, 14750);
+  useEffect(() => {
+    const loadInterval = setTimeout(() => {
+      setLoaded(true);
+    }, 250);
+    const fadeInterval = setTimeout(() => {
+      setLoaded(false);
+    }, 14750);
 
-            return () => {
-                setLoaded(false)
-                clearTimeout(loadInterval)
-                clearTimeout(fadeInterval)
-            }
-    }, [asset.alt])
+    return () => {
+      setLoaded(false);
+      clearTimeout(loadInterval);
+      clearTimeout(fadeInterval);
+    };
+  }, [asset.alt]);
 
-    if (asset.projectAsset.image.length > 0) {
-        projectAsset = (
-            <aside className='img-assets' id={asset.projectAsset.photoLayout} style={{
-                backgroundImage: asset.isFullScreen ? `url(${asset.projectAsset.image[0].asset.url})` : null,
-            }}>
-                {asset.projectAsset.image.map(img => {
-                    return <div key={img.asset.id} className={`img-asset${img === asset.projectAsset.image[0] ? ' mobile' : ''}${asset.isFullScreen ? ' fullscreen' : ''}`}><Img fluid={img.asset.fluid}  alt={asset.alt} /></div>
-                })}
-            </aside>
-        )
-    } else {
-        projectAsset = (
-            <Video
-                src={asset.projectAsset.video.asset.url}
-                className={`video ${asset.isFullScreen ? 'fullscreen' : ''}`}
-                onClick={onClick}
-            />
-        )
-    }
-    
-    return (
-            <section className={`project-asset${asset.isFullScreen ? ' fullscreen' : ''}${loaded ? ' visible' : ''}`}>
-                {projectAsset}
-            </section>
-    )
-}
-export default ProjectAsset
+  if (asset.projectAsset.image.length > 0) {
+    projectAsset = (
+      <aside
+        className="img-assets"
+        id={asset.projectAsset.photoLayout}
+        style={{
+          backgroundImage: asset.isFullScreen
+            ? `url(${asset.projectAsset.image[0].asset.url})`
+            : null
+        }}
+      >
+        {asset.projectAsset.image.map(img => {
+          return (
+            <div
+              key={img.asset.id}
+              className={`img-asset${
+                img === asset.projectAsset.image[0] ? " mobile" : ""
+              }${asset.isFullScreen ? " fullscreen" : ""}`}
+            >
+              <Img fluid={img.asset.fluid} alt={asset.alt} />
+            </div>
+          );
+        })}
+      </aside>
+    );
+  } else {
+    projectAsset = (
+      <Video
+        src={asset.projectAsset.video.asset.url}
+        className={`video ${asset.isFullScreen ? "fullscreen" : ""}`}
+        onClick={onClick}
+      />
+    );
+  }
+
+  return (
+    <>
+      <section
+        className={`project-asset${asset.isFullScreen ? " fullscreen" : ""}${
+          loaded ? " visible" : ""
+        }`}
+      >
+        {projectAsset}
+      </section>
+      <Footer isFullScreen={asset.isFullScreen} credits={asset.credits} />
+    </>
+  );
+};
+export default ProjectAsset;
