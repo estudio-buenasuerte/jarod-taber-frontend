9f45605e36149a86d4ecd587912dfd8032d0b2ba
Lucas Vocos <lucasvocos@gmail.com>
2020-09-09 13:50:52 -0400

    query for description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,27 @@
-import React from "react"
-import Portfolio from "../components/Portfolio/Portfolio"
-import SEO from "../components/seo"
+import React from 'react';
+import Portfolio from '../components/Portfolio/Portfolio';
+import SEO from '../components/seo';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const IndexPage = () => {
-	
+	const data = useStaticQuery(graphql`
+		{
+			allSanitySiteSettings {
+				nodes {
+					title
+					description
+				}
+			}
+		}
+	`);
+
+	const { title, description } = data.allSanitySiteSettings.nodes[0];
+
 	return (
 		<Portfolio>
-			<SEO title="JAROD TABER" />
+			<SEO title={title} description={description} />
 		</Portfolio>
-	  )
-}
+	);
+};
 
-export default IndexPage
+export default IndexPage;
